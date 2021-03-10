45bb469b3f3998f5c105b2def50198244ee7d895
TD <thea.dimovska@hotmail.com>
2021-03-10 19:29:24 +0100

    progress bar

--- app/src/main/java/com/example/cleanboost/MainActivity.java
+++ app/src/main/java/com/example/cleanboost/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.cleanboost;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+    Random rand = new Random();
+    int n = rand.nextInt(50);
+    //if( n < 30){
+
+    //};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +32,21 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_storage, R.id.navigation_cpu, R.id.navigation_home, R.id.navigation_vpn,R.id.navigation_battery)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        //action bar
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.opt_actionbar);
+        getSupportActionBar().setElevation(0);
+
+        ProgressBar simpleProgressBar=(ProgressBar) findViewById(R.id.simpleProgressBar); // initiate the progress bar
+        simpleProgressBar.setMax(100); // 100 maximum value for the progress bar
+        simpleProgressBar.setProgress(50); // 50 default progress value for the progress bar
+
     }
+
+
 
 }
