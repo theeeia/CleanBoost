990bf6cf856e84fb5a149fdae1e63ec3a7c53900
Dogsiskela <aleksandra.popovska@outlook.com>
2021-03-18 00:35:47 +0100

    scan screen icon update and bar

--- app/src/main/java/com/example/cleanboost/ui/home/ScanFragment.java
+++ app/src/main/java/com/example/cleanboost/ui/home/ScanFragment.java
@@ -11,10 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.cleanboost.R;
 import com.example.cleanboost.ui.cpu.CpuFragment;
 import com.example.cleanboost.ui.storage.StorageFragment;
+
+import java.util.Random;
 
 
 public class ScanFragment extends Fragment {
@@ -36,6 +40,29 @@ public class ScanFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ProgressBar simpleProgressBar= getView().findViewById(R.id.simpleProgressBar); // initiate the progress bar
+        TextView progressBarStatus = getView().findViewById(R.id.toolbar_status);
+        simpleProgressBar.setMax(100); // 100 maximum value for the progress bar
+
+        //Generate random number for now
+        //in the future it will be updated based on how long the user hasn't had a full scan
+        final int randomGeneratedStatus = new Random().nextInt(101) + 1;
+        simpleProgressBar.setProgress(randomGeneratedStatus); // 50 default progress value for the progress bar
+
+        if(randomGeneratedStatus<=40)
+        {progressBarStatus.setText(getResources().getString(R.string.status_poor));
+            progressBarStatus.setTextColor(getResources().getColor(R.color.activePrimary));
+            simpleProgressBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar));}
+
+        else if(randomGeneratedStatus<=80)
+        {progressBarStatus.setText(getResources().getString(R.string.status_ok));
+            progressBarStatus.setTextColor(getResources().getColor(R.color.primaryYellow));
+            simpleProgressBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar_ok));}
+        else
+        {progressBarStatus.setText(getResources().getString(R.string.status_good));
+            progressBarStatus.setTextColor(getResources().getColor(R.color.primaryGreen));
+            simpleProgressBar.setProgressDrawable(getResources().getDrawable(R.drawable.progressbar_good));}
 
         //Button navigation to the action screens
         final Button cpu_btn = view.findViewById(R.id.cpu_btn);
