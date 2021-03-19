11e5e798744c9677ae8ac9b645c98bb6abcb0642
Dogsiskela <aleksandra.popovska@outlook.com>
2021-03-19 23:31:08 +0100

    cpu animation

--- app/src/main/java/com/example/cleanboost/ui/cpu/CpuFragment.java
+++ app/src/main/java/com/example/cleanboost/ui/cpu/CpuFragment.java
@@ -1,5 +1,6 @@
 package com.example.cleanboost.ui.cpu;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,10 +26,27 @@ public class CpuFragment extends Fragment {
 
         return  inflater.inflate(R.layout.fragment_cpu, container, false);
     }
-
+    private void startCountAnimation(TextView numberAnimated, int from, int to) {
+        ValueAnimator animator = ValueAnimator.ofInt(from, to);
+        animator.setDuration(2000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                numberAnimated.setText(animation.getAnimatedValue().toString()+"%");
+            }
+        });
+        animator.start();
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        //Animate cpuNumber
+        TextView cpuNumber = view.findViewById(R.id.cpu_number);
+        startCountAnimation(cpuNumber,0,88);
+
+        //Animate ram number
+        TextView ramNumber = view.findViewById(R.id.ram_number);
+        startCountAnimation(ramNumber,0,76);
+
 
 
         // Image on click listener
